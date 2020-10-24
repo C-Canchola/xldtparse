bf6fff8d945341b7c43e53bee1dc31086dbe57cd
cancholacaleb <35677653+cancholacaleb@users.noreply.github.com>
2020-10-24 15:47:10 -0700

    Added encompassing parse method and test

--- parse_test.go
+++ parse_test.go
@@ -3,6 +3,7 @@ package xldtparse
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestMMDDYYParse(t *testing.T) {
@@ -21,4 +22,36 @@ func TestFloatStringParse(t *testing.T) {
 		t.Error(err)
 	}
 	fmt.Println(d.Date())
+}
+func TestParse(t *testing.T) {
+	firstS := "01-22-20"
+	d, err := ParseExcelString(firstS)
+	if err != nil {
+		t.Error(err)
+	}
+	if d.Day() != 22 {
+		t.Error("first day should be 22")
+	}
+	if d.Year() != 2020 {
+		t.Error("first year should be 2020")
+	}
+	if d.Month() != time.Month(1) {
+		t.Error("first month should be 1")
+	}
+
+	secondS := "44015.933692129598"
+	d2, err := ParseExcelString(secondS)
+
+	if err != nil {
+		t.Error(err)
+	}
+	if d2.Day() != 3 {
+		t.Error("first day should be 22")
+	}
+	if d2.Year() != 2020 {
+		t.Error("first year should be 2020")
+	}
+	if d2.Month() != time.Month(7) {
+		t.Error("first month should be 7")
+	}
 }
